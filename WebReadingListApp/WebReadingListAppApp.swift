a68102ae90ebfc3342f56f5947f0ec74360bbdf6
ewrika <egorkaomsk_2003@mail.ru>
2024-11-29 15:01:20 +0600

    Initial Commit

---
+++ WebReadingListApp/WebReadingListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebReadingListAppApp.swift
+//  WebReadingListApp
+//
+//  Created by Георгий Борисов on 29.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct WebReadingListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
